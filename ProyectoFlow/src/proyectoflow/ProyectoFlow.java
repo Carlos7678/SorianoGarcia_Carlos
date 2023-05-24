1ff41a50e491379877510971630ef55d62e97ec6
Carlos <117821354+Carlos7678@users.noreply.github.com>
2023-05-24 11:30:52 +0200

    commit inicial

---
+++ ProyectoFlow/src/proyectoflow/ProyectoFlow.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoflow;
+
+/**
+ *
+ * @author Alumno
+ */
+public class ProyectoFlow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
